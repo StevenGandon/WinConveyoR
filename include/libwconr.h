52ecf8ab5dfa644d87faa24baa9f004aed0c238a
StevenGandon <steven.gandon@epitech.eu>
2025-03-10 22:58:07 +0100

    [ADD] base things in lib headerfile

--- include/libwconr.h
+++ include/libwconr.h
@@ -1,4 +1,47 @@
 #ifndef LIBWCONR_H_
     #define LIBWCONR_H_
 
+    /* ==== enums ==== */
+
+    /* architectures */
+    enum SUPPORTED_ARCHITECTURES {
+        UNSUPPORTED_ARCH = -(1 << 0),
+        X86_64_ARCH = (1 << 0),
+        ARM64_ARCH = (1 << 1),
+        I686_ARCH = (1 << 2)
+    };
+
+    /* os */
+    enum SUPPORTED_PLATFORMS {
+        UNSUPPORTED_PLTF = -(1 << 0),
+        NT_PLTF = (1 << 0),
+        DARWIN_PLTF = (1 << 1),
+        GEN_LINUX_PLTF = (1 << 2)
+    };
+
+    /* ==== structs definition ==== */
+
+    /* current system infos */
+    struct wcr_system_s {
+        short arch;     // system architecture
+        short platform; // system operating system
+    };
+
+    /* current state datas of the program */
+    struct wcr_state_s {
+        struct wcr_system_s system_informations; // system information about current machine / target machine
+    };
+
+    /* ==== types definition ==== */
+
+    typedef struct wcr_state_s wcr_state;
+    typedef struct wcr_system_s wcr_system;
+
+    /* ==== high level interfaces ====  */
+
+    wcr_state *new_state(void);
+    void close_state(wcr_state *__s);
+
+    /* ==== low level interfaces ==== */
+
 #endif /* !LIBWCONR_H_ */
---
+++ lib/libwconr/src/wcr_state.c
@@ -0,0 +1,22 @@
+#include "libwconr.h"
+
+#include <stdlib.h>
+
+wcr_state *
+new_state(void)
+{
+    wcr_state *state = (wcr_state *)malloc(sizeof(wcr_state));
+
+    if (!state)
+        return (NULL);
+    return (state);
+}
+
+void
+close_state(state)
+    wcr_state *state;
+{
+    if (!state)
+        return;
+    free(state);
+}
