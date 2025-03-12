f443ea37cfcfc1a8744ae94bd08d861a17b0e8e0
StevenGandon <steven.gandon@epitech.eu>
2025-03-12 17:26:55 +0100

    [ADD] prototypes for next function

--- lib/libwconr/libwconr_private.h
+++ lib/libwconr/libwconr_private.h
@@ -26,6 +26,20 @@
         unsigned char *value;
     };
 
+    struct _http_request_parser_s {
+        struct _http_connection_s *client;
+
+        unsigned char *method;
+
+        unsigned char *route;
+
+        unsigned char *protocol;
+        unsigned short version;
+
+        struct _http_header_s **headers;
+        size_t header_size;
+    };
+
     struct _http_response_parser_s {
         struct _http_connection_s *client;
 
@@ -40,8 +54,15 @@
     };
 
     void end_http_response_parser(struct _http_response_parser_s *);
+    void end_http_request_parser(struct _http_request_parser_s *);
     void end_http_connection(struct _http_connection_s *);
     struct _http_connection_s *new_http_connection(const char *__s, int);
     struct _http_response_parser_s *new_http_response_parser(struct _http_connection_s *);
+    struct _http_request_parser_s *new_http_request_parser(struct _http_connection_s *);
+    void set_header(struct _http_header_s ***__h, char *__k, char *__v);
+    char *get_header(const struct _http_header_s **__h, char *__k);
+    void get_chunk(struct _http_connection_s *__c, size_t __s, unsigned char *__d);
+    void request_ressource(struct _http_request_parser_s *__r);
+    void fetch_response(struct _http_response_parser_s *__r);
 
 #endif /* !LIBWCONR_PRIVATE_H_ */
