44fd27d5b948324e7757f18128bccb3f1c75834d
th0m <91764036+thomasvsl@users.noreply.github.com>
2026-05-05 10:39:57 +0100

    [FIX] Windows build

--- include/libwconr.h
+++ include/libwconr.h
@@ -4,6 +4,9 @@
     #include <stddef.h>
 
     #ifdef _WIN32
+        #ifndef WIN32_LEAN_AND_MEAN
+            #define WIN32_LEAN_AND_MEAN
+        #endif
         #include <windows.h>
         typedef CRITICAL_SECTION wcr_mutex;
     #else
