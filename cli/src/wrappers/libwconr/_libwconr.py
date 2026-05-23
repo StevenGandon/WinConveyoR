29560749cbfa109a7d892e392440d13da96673bb
th0m <91764036+thomasvsl@users.noreply.github.com>
2026-05-23 20:03:43 +0100

    [FIX] langpack imports

--- cli/src/wrappers/libwconr/_libwconr.py
+++ cli/src/wrappers/libwconr/_libwconr.py
@@ -1,4 +1,4 @@
-from ctypes import Structure, POINTER, c_short, c_ushort, c_int, c_uint, c_long, c_ulong, c_double, c_float, c_size_t, c_ssize_t
+from ctypes import Structure, POINTER, c_short, c_ushort, c_int, c_uint, c_long, c_ulong, c_double, c_float, c_size_t, c_ssize_t, c_void_p, c_char_p
 from ctypes import c_byte as c_char
 from ctypes import c_ubyte as c_uchar
 from enum import Enum
@@ -39,7 +39,7 @@ class wcr_source_s(Structure):
     pass
 
 wcr_source_s._fields_ = [
-    ("url", POINTER(c_char)),
+    ("url", c_char_p),
     ("proto", c_int)
 ]
 
@@ -48,8 +48,8 @@ class wcr_state_s(Structure):
 
 wcr_state_s._fields_ = [
     ("system_informations", wcr_system_s),
-    ("cache_path", POINTER(c_char)),
-    ("config_path", POINTER(c_char)),
+    ("cache_path", c_char_p),
+    ("config_path", c_char_p),
     ("sources", POINTER(POINTER(wcr_source_s))),
     ("sources_count", c_size_t),
     ("lock", c_char * 64),
@@ -96,11 +96,11 @@ class Mapper(object):
             self.init_mapper()
         self._dll.register_function("new_state", POINTER(wcr_state_s))
         self._dll.register_function("close_state", None, POINTER(wcr_state_s))
-        self._dll.register_function("download_package", c_int, POINTER(c_char), POINTER(c_char))
-        self._dll.register_function("write_state", c_int, POINTER(wcr_state_s), POINTER(c_char))
-        self._dll.register_function("load_state", POINTER(wcr_state_s), POINTER(c_char))
-        self._dll.register_function("wcr_state_add_source", c_int, POINTER(wcr_state_s), c_int, POINTER(c_char))
-        self._dll.register_function("sync_package_list", c_int, POINTER(wcr_state_s), c_int, POINTER(c_char))
-        self._dll.register_function("install_package", c_int, POINTER(wcr_state_s), c_int, POINTER(c_char), POINTER(c_char))
+        self._dll.register_function("download_package", c_int, c_char_p, c_char_p)
+        self._dll.register_function("write_state", c_int, POINTER(wcr_state_s), c_char_p)
+        self._dll.register_function("load_state", POINTER(wcr_state_s), c_char_p)
+        self._dll.register_function("wcr_state_add_source", c_int, POINTER(wcr_state_s), c_int, c_char_p)
+        self._dll.register_function("sync_package_list", c_int, POINTER(wcr_state_s), c_int, c_char_p)
+        self._dll.register_function("install_package", c_int, POINTER(wcr_state_s), c_int, c_char_p, c_char_p)
         self._dll.register_function("wcr_set_event_callback", None, POINTER(wcr_state_s), c_void_p, POINTER(None))
 
