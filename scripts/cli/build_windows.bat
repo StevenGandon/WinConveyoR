5804b0c391cba994de4331a52f23c6338b8473ef
StevenGandon <steven.gandon@epitech.eu>
2026-09-25 13:52:51 +0200

    [TEST] CI/CD

--- scripts/cli/build_windows.bat
+++ scripts/cli/build_windows.bat
@@ -1,6 +1,7 @@
 
 set DIST_PATH=dist\windows-amd64\bin
 set BUILD_PATH=build\windows-amd64\pyinstaller
+setlocal EnableDelayedExpansion
 
 mkdir %DIST_PATH%
 mkdir %BUILD_PATH%
@@ -13,7 +14,8 @@ if not exist "dist/windows-amd64/lib/libwconr.dll" (
 set "BINARY_ARGS="
 
 for %%F in ("dist\windows-amd64\lib\*.dll") do (
-    set "BINARY_ARGS=!BINARY_ARGS! --add-binary "..\..\..\%%~fF:.""
+    set "BINARY_ARGS=!BINARY_ARGS! --add-binary "..\..\..\%%F:.""
 )
 
 pyinstaller .\cli\main.py -c -F -n wcr -y --distpath %DIST_PATH% --workpath %BUILD_PATH% --specpath %BUILD_PATH% --optimize 2 --add-data "../../../assets/cli:./assets/cli" !BINARY_ARGS!
+endlocal
